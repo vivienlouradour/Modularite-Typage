f8abf6083bfdeaf0906f5610a53c24d1c46ad67d
Vivien Louradour <vivien.louradour@hotmail.fr>
2017-10-12 10:29:36 +0200

    exercice

--- session1/exercices/ResolutionLiaison.java
+++ session1/exercices/ResolutionLiaison.java
@@ -8,11 +8,16 @@ class C extends B {}
 
 class X {
 	void f(A a){
-		System.out.println("X.f(A)");
+		System.out.println("X.f(A)"); //Pas exécutée car type A moins proche de C que B
 	}
 	void f(B b){
-		System.out.println("X.f(B)");
+		System.out.println("X.f(B)"); //Méthode exécutée
 	}
+	/*
+	void f(C c) {
+		System.out.println("X.f(C)");
+	}
+	 */
 }
 
 class Y extends X {
@@ -26,9 +31,7 @@ public class ResolutionLiaison {
 		Y y = new Y();
 		C c = new C();
 		y.f(c);
-		X x = y;
+		X x = y; //Execute la méthode f(B b) de la classe X. Sauf si la méthode X.f(C c) est décommentée. Dans ce cas là la méthode redéfinie (Y.f(C)) est choisie lors de l'exécution
 		x.f(c);;
 	}
-	
-	
 }
